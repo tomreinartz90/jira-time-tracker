82b2f2adcba046e88f581c595226cbc72fd16561
Tom Reinartz <tom.reinartz@dotcontrol.nl>
2018-10-19 13:58:53 +0200

    remove old code

--- src-old/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'seed-app',
-    templateUrl: './app.component.html',
-})
-export class AppComponent {}
-
-
--- src-old/app/utils/date.util.ts
+++
@@ -1,9 +0,0 @@
-import {groupBy} from 'lodash';
-
-export class DateUtil {
-    static groupByDateDay(collection: Array<any>, dateSelector: Function) {
-        return groupBy(collection, (item) => {
-            return new Date(dateSelector(item)).toISOString().slice(0, 10);
-        });
-    }
-}
