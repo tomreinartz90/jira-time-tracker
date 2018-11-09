9f90be11113cbb225c02c9f67d314e185f591ba1
Martijn Hoogerland <martijn.hoogerland@dotcontrol.com>
2018-11-09 22:18:01 +0100

    Added menubar icon (tray icon on Windows) and removed title bar on Mac

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
-import {app, BrowserWindow, screen} from 'electron';
+import {app, BrowserWindow, screen, Tray, nativeImage} from 'electron';
 import * as path from 'path';
 import * as url from 'url';
+import { createPlatformFactory } from '@angular/core';
 
-let win, serve;
+let base64Icon = `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAYAAADEtGw
+7AAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4AkZCg87wZW7ewA
+AAp1JREFUOMuV1U2IVlUcx/HPnbc0MWwEF40hRWRQmWhEUi4KorlTQ0zQKgqSxKinRYuWrdq0iIp8DAy
+CFmYUUVTYY0Qw0SsYVDQRlFlQU4o4VDMUY9NzWtz/45znzo3yv7n/l3O+53fOPS+F/7R9G0l34Vlap/x
+PG+gPby76471jpJdxI4p/x5QrakPVZ3yI4lLSLH4LpetIT5N24AWKpZXAW4boXogFnGxQXEzhdQYHl0v
+pbtJkBIOkBqXpVhzAWIPi8hocxCyH5qp0e10oHY6BNy3P7szULyc9hzkGTjat8WPRqctkD3QORrJ211J
+srPV7CKP4i7S6CXxF+GtY2lG5D5yg+D6bckHaRXs463dV+OtJVzeBj4Q/inuy2uf4NYPvyVR38Vn4GzD
+ZAC5ezHbITsqtEU8HvGcjpFblDncpDma16yhvqit+c3mLuQj3Vm7rJ4r3kW+z+6sD80aKQWcivwm318B
+pHk9mA11PuSXil/B1thyrSA9HMI8nMtYNlDszcKdbHVcLkduCO0L1VxTv1VTv5plR3lrCuzga+c2YqB2
+QNEfqjV7EWl8c8X78kKleTTfWeuA49maDjlNuz8CHFykOYDEabKvg0Jqh+AB/Z4D7qs+h03gbxyK/FVf
+WL6FfsC/8tdGoZ0/hRKZ6A+2pUP1jdZecse01cGcBr2YNzqdcG6q/oDgS+7e3XLeF6j/wTvzM6Lfi2nQ
+KP8e0P6Ezn9X2488MvLnW75vwP2wCr8J5eD4upsxaHZzOwNNZcU2c3FfwWg1cDuISfIxH6fzedE8G90s
+8nuXH8B0eoXNc/6tQjsQfXaQz0/BEXUD3W4oF0hQPflTlJwZIl+FcOp86e2vvoj1Le6I/P974ZA2dBXk
+97qQ13Z8+3PS0+AdjKa1R95YOZgAAAABJRU5ErkJggg==`
+
+let win, serve, tray;
 const args = process.argv.slice(1);
 serve = args.some(val => val === '--serve');
 
@@ -21,7 +37,8 @@ function createWindow() {
     minHeight: 600,
     resizable: true,
     skipTaskbar: false,
-    title: 'Simplicate Time Tracker'
+    titleBarStyle: 'hiddenInset',
+    title: 'Simplicate Time Tracker',
   });
 
   if (serve) {
@@ -49,12 +66,47 @@ function createWindow() {
 
 }
 
+function createTray() {
+  tray = new Tray(path.join(__dirname, 'dist/assets/menubar.png'));
+  tray.setToolTip('Simplicate Time Tracker');
+
+  tray.on('click', toggleWindow);
+}
+
+function toggleWindow() {
+  const trayPos = tray.getBounds();
+  const windowPos = win.getBounds();
+
+  let x, y = 0
+  if (process.platform == 'darwin') {
+    x = Math.round(trayPos.x + (trayPos.width / 2) - (windowPos.width / 2))
+    y = Math.round(trayPos.y + trayPos.height)
+  } else {
+    x = Math.round(trayPos.x + (trayPos.width / 2) - (windowPos.width / 2))
+    y = Math.round(trayPos.y + trayPos.height * 10)
+  }
+
+  win.setPosition(x, y, false)
+
+  if(win.isVisible()) {
+    win.hide();
+  }
+  else {
+    win.show();
+    win.focus();
+  }
+}
+
+
 try {
 
   // This method will be called when Electron has finished
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
-  app.on('ready', createWindow);
+  app.on('ready', () => {
+    createWindow();
+    createTray();
+  });
 
   // Quit when all windows are closed.
   app.on('window-all-closed', () => {
